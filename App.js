64fb145bd49e7802e803dd6342e0c030927f8d7f
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-06-07 10:43:57 +0530

    Lecture - 4 - Laying the Foundation

--- App.js
+++ App.js
@@ -1,30 +1,51 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
+// HTML using React.createElement() --> Replaced by JSX
+// const heading = React.createElement(
+//     "h1",
+//     {
+//       id: "title",
+//       key: "h1",
+//     },
+//     "heading"
+//   );
+//   const heading1 = React.createElement(
+//     "h1",
+//     {
+//       id: "title2",
+//       key: "h2",
+//     },
+//     "heading1"
+//   );
+  
+//   const container = React.createElement(
+//     "div",
+//     {
+//       id: "container",
+//     },
+//     [heading, heading1]
+//   );
+  
 
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading"
+//JSX - tocreateHTML
+const heading = <h1>Hello World</h1>;
+const heading2 = (
+  <h1 id = "title" key = "heading">
+    Hello World Again!
+  </h1>
+)
+
+//Functional Components
+const HeaderComponent = () => {
+  return (
+    <div>
+      {heading2}
+      <h2>Namaste React</h2>
+      <h2>Hi All</h2>
+    </div>
   );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title2",
-    },
-    "heading1"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading1]
-  );
-  
+};
   // create root using createRoot
   const root = ReactDOM.createRoot(document.getElementById("root"));
   // passing react element inside root
-  root.render(container);
+  root.render(<HeaderComponent />);
